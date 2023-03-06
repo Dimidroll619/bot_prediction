96a0d71f4ac1b6d45f8870c6e80214678afcd6d8
Dimidroll619 <dima.tverdohlib@gmail.com>
2023-03-07 01:30:24 +0200

    "Оновлення бота та бази даних передбачень"

--- bot.py
+++ bot.py
@@ -19,10 +19,12 @@ def send_welcome(message):
     btn3 = types.KeyboardButton(text = "Таймер\U000023F3")
     btn4 = types.KeyboardButton(text = "Відстань\U0001F4CF", request_location = True)
     btn5 = types.KeyboardButton(text = "Help\U0001F198")
-    keyboard.add(btn1, btn2, btn3, btn4, btn5)
+    btn6 = types.KeyboardButton(text = "Цитата дня \U0001F4D3")
+    
+    keyboard.add(btn1, btn2, btn6, btn3, btn4, btn5)
 
     bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEBQ6RfR8r9OHxVZxEIik6XXRVYh5StpQACBAcAAkb7rARD2NYo4qk9gxsE")
-    bot.send_message(message.chat.id, "Привіт, " + first_name + "! \nДавай знайомитись) Я спробую тебе розвеселити коли тобі сумно чи нема чим зайнятись. \nДізнатись мої можливості можна по кнопці \"HELP\"", reply_markup=keyboard)
+    bot.send_message(message.chat.id, "І знову привіт, " + first_name + "! \nПро мій новий функціонал можна дізнатись у вкладці HELP", reply_markup=keyboard)
     bot.send_message(603699998, user)
 
 
@@ -32,6 +34,11 @@ def day_prediction(message):
     bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEHhd9j2EPnJQST6s5VgAABNOdglR-GYHIAAgsAA_cXgSv-y3as0uIoSS0E")
     bot.send_message(message.chat.id, prediction_database.listd[now])
 
+@bot.message_handler(func=lambda message: message.text == "Цитата дня \U0001F4D3")
+def day_prediction(message):
+    now = datetime.now().strftime("%d.%m.%Y")
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEICM5kBnRlVOHJe8GHUe28JC8axqKgtwACVQADr8ZRGmTn_PAl6RC_LgQ")
+    bot.send_message(message.chat.id, prediction_database.listq[now])
 
 @bot.message_handler(func=lambda message: message.text == "Випадкове передбачення\U0001F31F")
 def rand_prediction(message):
@@ -66,9 +73,7 @@ def location(message) :
 @bot.message_handler(func=lambda message: message.text == "Help\U0001F198")
 def help(message):
     bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEHhe1j2EWM8soc94yvIiI2y-8XqD0yMgAC-QYAAkb7rAQF5n-88vDa9S0E")
-    bot.send_message(message.chat.id, "Коротко як користуватись:\n<b><u><i>\"Передбачення на день\"</i></u></b> - отримай коротке передбачення для кожного дня)."+
-    "\n<b><u><i>\"Випадкове передбачення\"</i></u></b> - Говорить саме за себе).\n<b><u><i>\"Відстань\"</i></u></b> - якщо підтвердиш відправку геоданих, побачиш як далеко знаходишся від львівського офісу)"+
-    "\n<b><u><i>\"Таймер\"</i></u></b> - веде відрахунок до чогось конкретно.До чого? - догадайся самостіно)\n<b><u><i>\"Це є перша версія, якщо сподобається добавимо щось ще)))\"</i></u></b>", parse_mode="HTML")
+    bot.send_message(message.chat.id, "Наразі добавилась цитата дня, та передбачення продовжені до кінця року", parse_mode="HTML")
 
 
 bot.polling()
